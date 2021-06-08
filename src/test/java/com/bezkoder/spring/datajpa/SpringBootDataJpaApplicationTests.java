0ed9522530b1972bfd43bf9944c8cc2dadc96970
Beyond-Logic <edemaero@gmail.com>
2021-06-08 11:52:10 +0100

    edit test class

--- src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -1,13 +1,13 @@
 //package com.bezkoder.spring.datajpa;
 //
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 //@SpringBootTest
-//class SpringBootDataJpaApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
